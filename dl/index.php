8835e4c71a1bd7e2dac2aaf60b83991e3f5a662f
Bernhard Weichel <github.com@nospam.weichel21.de>
2015-12-09 16:28:09 +0100

    fix encoding

--- dl/index.php
+++ dl/index.php
@@ -87,7 +87,7 @@ switch ( $_POST[ "action" ] )
 		$headers = array();
 
 		$headers[] = "MIME-Version: 1.0";
-		$headers[] = "Content-type: text/html; charset=iso-8859-1";
+		$headers[] = "Content-type: text/html; charset=UTF-8";
 		$headers[] = "To: $to";
 		$headers[] = "Subject: $subject";
 
